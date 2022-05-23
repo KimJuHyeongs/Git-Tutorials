68909ed0c6a1da76e70ad5c34f4d1b7958c13490
KimJuHyeongs <wngud01271@gmail.com>
2022-05-23 22:00:34 +0900

    Add sign up system & login, And Fix Korean Bug, login index problem and break in switch

--- scheduler.c
+++ scheduler.c
@@ -28,16 +28,20 @@ int main(void){
 
         switch(input){
             case 0:
-               exit(0);
+                exit(0);
+                break;
             case 1:
                 enroll_num();
+                break;
             case 2:
                 login_out();
+                break;
             case 4:
                 disp_calendar();
                 break;
           default:
-               printf("Wrong Enter.\nPlease Enter again.\n");
+               printf("Wrong Enter.\nPlease Enter again.\n\n");
+               break;
         }
     }
 
@@ -45,14 +49,14 @@ int main(void){
 
 int disp_menu(){
     int tmp;
-    printf("-------------[ ?쉶?쓽?떎 ?삁?빟 ?떆?뒪?뀥 ]-------------\n");
-    printf("1. ?궗踰? ?벑濡?/?궘?젣\n");
-    printf("2. 濡쒓렇?씤/濡쒓렇?븘?썐\n");
-    printf("3. ?쉶?쓽?떎 ?삁?빟\n");
-    printf("4. ?삁?빟 ?긽?깭 議고쉶\n");
-    printf("0. 醫낅즺\n");
+    printf("-------------[ 회의실 예약 시스템 ]-------------\n");
+    printf("1. 사번 등록/삭제\n");
+    printf("2. 로그인/로그아웃\n");
+    printf("3. 회의실 예약\n");
+    printf("4. 예약 상태 조회\n");
+    printf("0. 종료\n");
     printf("-----------------------------------------------\n");
-    printf("> 踰덊샇 ?꽑?깮 : ");
+    printf("> 번호 선택 : ");
     fflush(stdin);
     scanf("%d",&tmp);
     return tmp;
@@ -73,7 +77,7 @@ void disp_calendar(){
         total += mdays[idx];
     }
     if(leap_check(y) && (m>2)) total++;
-    total++;    //1?씪 ?늻?쟻
+    total++;    
 
     start = total%7;
     day = mdays[m-1];
@@ -102,28 +106,29 @@ int leap_check(const int y){
 }
 
 void enroll_num(){
-    char *tmp_id, *tmp_pw;
+    char tmp_id[100], tmp_pw[100];
     int idx;
 
     if(login_check == 1){
-        printf("@ 濡쒓렇?븘?썐 ?썑 ?궗?슜?븷 ?닔 ?엳?뒿?땲?떎!\n");
+        printf("@ 로그아웃 후 사용할 수 있습니다!\n");
         return;
     }
     if(cur_user >= MAX_USER){
-        printf("@ ?벑濡앸맂 ?씤?썝?씠 珥덇낵?릺?뿀?뒿?땲?떎.\n");
+        printf("@ 아이디는 최대 %d개 등록할 수 있습니다.\n",MAX_USER);
         return;
     }
 
-    printf("?궗踰덉쓣 ?엯?젰?븯?꽭?슂 : ");
+    printf("아이디를 입력하세요 : ");
     fflush(stdin);
-    fgets(tmp_id,20,stdin);
+    fgets(tmp_id,100,stdin);
     tmp_id[strlen(tmp_id)-1] = '\0';
+    
     if(strlen(tmp_id)>MAX_ID){
-        printf("?궗踰덉쓽 湲몄씠?뒗 %d ?씠?븯 ?엯?땲?떎.\n",MAX_ID);
+        printf("아이디의 길이는 %d이하로 입력하세요.\n",MAX_ID);
         return;
     }
 
-    for(idx=0;idx<cur_user-1;idx++){
+    for(idx=0;idx<cur_user;idx++){
         if(!strcmp(user_id[idx],tmp_id)){
             deleteID(idx);
             return;
@@ -131,12 +136,12 @@ void enroll_num(){
     }
     
 
-    printf("鍮꾨??踰덊샇瑜? ?엯?젰?븯?꽭?슂 : ");
+    printf("비밀번호를 입력하세요 : ");
     fflush(stdin);
-    fgets(tmp_pw,20,stdin);
+    fgets(tmp_pw,100,stdin);
     tmp_pw[strlen(tmp_pw)-1] = '\0';
     if(strlen(tmp_pw) > MAX_PW){
-        printf("鍮꾨??踰덊샇?쓽 湲몄씠?뒗 %d ?씠?븯 ?엯?땲?떎. \n",MAX_PW);
+        printf("비밀번호의 길이는 %d이하로 입력하세요. \n",MAX_PW);
         return;
     }
     
@@ -144,8 +149,8 @@ void enroll_num(){
     strcpy(user_pw[cur_user],tmp_pw);
     cur_user++;
     
-    printf("?벑濡앸맂 ?궗踰? ?젙蹂?\n");
-    for(idx=0;idx<cur_user-1;idx++) printf("%s  ",user_id[idx]);
+    printf("등록된 사번 목록 : \n");
+    for(idx=0;idx<cur_user;idx++) printf("%s  ",user_id[idx]);
     printf("\n");
     
 }
@@ -153,58 +158,69 @@ void enroll_num(){
 void deleteID(const int del_idx){
     int idx;
     char del_check;
+    printf("del_idx = %d\n",del_idx);
 
-    printf("?씠誘? ?벑濡앸맂 ?궗踰덉엯?땲?떎.\n?벑濡앸맂 ?븘?씠?뵒瑜? ?궘?젣?븯?떆寃좎뒿?땲源?? Y/N ");
+    printf("이미 등록된 아이디입니다.\n해당 아이디를 삭제하시겠습니까? Y/N ");
+    fflush(stdin);
     scanf("%c",&del_check);
     if(del_check == 'y' || del_check =='Y'){
-        for(idx=del_idx;idx<cur_user-2;idx++){
-            strcpy(user_id[idx], user_id[idx+1]);
-            strcpy(user_pw[idx],user_pw[idx+1]);
+        if(cur_user == 1){
+            strcpy(user_id[del_idx], "\0");
+            strcpy(user_pw[del_idx],"\0");
             cur_user--;
-            printf("@ ?궘?젣 ?릺?뿀?뒿?땲?떎!\n");
         }
+        else{
+            for(idx=del_idx;idx<cur_user-1;idx++){
+                strcpy(user_id[idx], user_id[idx+1]);
+                strcpy(user_pw[idx],user_pw[idx+1]);
+            }
+            cur_user--;
+            strcpy(user_id[cur_user], "\0");
+            strcpy(user_pw[cur_user],"\0");
+        }
+        printf("@ 아이디가 삭제되었습니다!\n");
     }
-    printf("?벑濡앸맂 ?궗踰? ?젙蹂?\n");
-    for(idx=0;idx<cur_user-1;idx++) printf("%s  ", user_id[idx]);
+    printf("등록된 사번 목록 : \n");
+    for(idx=0;idx<cur_user;idx++) printf("%s  ", user_id[idx]);
     printf("\n");
 }
 
 void login_out(){
-    char *tmp_id, *tmp_pw;
+    char tmp_id[100], tmp_pw[100];
     int tmp_login_check = 0;
 
     if(login_check==1){
-        printf("# 濡쒓렇?븘?썐 ?릺?뿀?뒿?땲?떎!\n");
+        printf("# 로그아웃 되었습니다!\n");
         login_check = 0;
         return;
     }
 
-    printf("?궗踰덉쓣 ?엯?젰?븯?꽭?슂 : ");
+    printf("아이디를 입력하세요 : ");
     fflush(stdin);
-    fgets(tmp_id,MAX_ID+1,stdin);
+    fgets(tmp_id,100,stdin);
     tmp_id[strlen(tmp_id)-1] = '\0';
     tmp_login_check = is_enroll_num(tmp_id);
-    if(tmp_login_check){
-        printf("@ %s?뒗 ?벑濡앸릺吏? ?븡??? ?궗踰덉엯?땲?떎.\n",tmp_id);
+    if(tmp_login_check < 0){
+        printf("@ %s는 등록되지 않은 아이디입니다.\n",tmp_id);
         return;
     }
     
-    printf("鍮꾨??踰덊샇瑜? ?엯?젰?븯?꽭?슂 : ");
+    printf("비밀번호를 입력하세요 : ");
     fflush(stdin);
-    fgets(tmp_pw,MAX_PW+1,stdin);
+    fgets(tmp_pw,100,stdin);
     tmp_pw[strlen(tmp_pw)-1] = '\0';
     if(strcmp(user_pw[tmp_login_check],tmp_pw)){
-        printf("@ 鍮꾨??踰덊샇媛? ????졇?뒿?땲?떎!\n");
+        printf("@ 비밀번호가 잘못되었습니다!\n");
         return;
     }
 
-    printf("# 濡쒓렇?씤 ?릺?뿀?뒿?땲?떎!\n");
+    printf("# 로그인 되었습니다!\n");
     login_check = 1;
 }
 
 int is_enroll_num(const char* id){
     int idx;
 
-    for(idx=0;idx<MAX_USER;idx++) if(!strcmp(user_id[idx], id)) return idx;
-    return 0;
+    for(idx=0;idx<cur_user;idx++) if(!strcmp(user_id[idx], id)) return idx;
+    return -1;
 }
